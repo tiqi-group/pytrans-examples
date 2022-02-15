52b1d2dcb60339661e6a41baef0219f04e67ae0b
cmordini <cmordini@phys.ethz.ch>
2022-02-15 10:40:48 +0100

    Use qinterp in example

--- models/interp/loader.py
+++ models/interp/loader.py
@@ -6,23 +6,22 @@
 
 import numpy as np
 from pathlib import Path
-# from qinterp import tricubic
-from scipy.interpolate import RegularGridInterpolator
+from qinterp import tricubic
+# from scipy.interpolate import RegularGridInterpolator
 from functools import wraps
 
 
 data_path = Path(__file__).parent / 'data' / 'data3d.npy'
 data = np.load(data_path)  # columns: x, y, z, V_E2, Vps
-shape = (81, 51, 51)
 
-# interp_dc = tricubic(data[:, [0, 1, 2, 3]])
-# interp_ps = tricubic(data[:, [0, 1, 2, 4]])
-xyz = tuple(map(np.unique, data[:, :3].T))
-shape = tuple(map(len, xyz))
-print('-------- load:', shape)
+interp_dc = tricubic(data[:, [0, 1, 2, 3]])
+interp_ps = tricubic(data[:, [0, 1, 2, 4]])
+# xyz = tuple(map(np.unique, data[:, :3].T))
+# shape = tuple(map(len, xyz))
+# print('-------- load:', shape)
 
-interp_dc = RegularGridInterpolator(xyz, data[:, 3].reshape(shape))
-interp_ps = RegularGridInterpolator(xyz, data[:, 4].reshape(shape))
+# interp_dc = RegularGridInterpolator(xyz, data[:, 3].reshape(shape))
+# interp_ps = RegularGridInterpolator(xyz, data[:, 4].reshape(shape))
 
 
 def vectorize(func):
@@ -36,11 +35,11 @@ def vectorize(func):
     return wrapper
 
 
-# def moment0(x, y, z):
-#     return vectorize(interp_dc)(x, y, z)
-
 def moment0(x, y, z):
-    return interp_dc((x, y, z))
+    return vectorize(interp_dc)(x, y, z)
+
+# def moment0(x, y, z):
+#     return interp_dc((x, y, z))
 
 
 def moment_ps(x, y, z):

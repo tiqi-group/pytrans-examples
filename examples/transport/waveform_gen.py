cfe7e81c71e775379bdeddfdb17d2f2aeebb688b
cmordini <cmordini@phys.ethz.ch>
2023-06-16 17:14:23 +0200

    Filter compensation example
    Former-commit-id: 2dccf9896ad0b2cc30117de1e0348e85d5472927

--- examples/transport/waveform_gen.py
+++ examples/transport/waveform_gen.py
@@ -10,7 +10,7 @@ from pytrans import objectives as obj
 from pytrans.conversion import freq_to_curv
 from pytrans.ions import Ca40
 from pytrans.functions import zpspace
-from pytrans.solver import solver
+from pytrans.solver import solver, init_waveform
 
 from models.surface_trap import SurfaceTrap
 from pathlib import Path
@@ -25,33 +25,32 @@ if __name__ == '__main__':
     axial = freq_to_curv(1e6, ion=Ca40)
     rtilt = 8e7
 
-    x = zpspace(trap.x_ele(2), trap.x_ele(4), 50)
+    n_samples = 50
+    x = zpspace(trap.x_ele(2), trap.x_ele(4), n_samples)
     y0 = 0
     z0 = trap.z0
 
-    # len(step_opjectives) == number of steps == number of samples in the final waveform
-    # each element is a list of minimization objectives for that step
-    step_objectives = [
-        [
+    waveform = init_waveform(n_samples, trap.n_electrodes)
+
+    objectives = []
+
+    # fill objectives per-step
+    for j, x0 in enumerate(x):
+        objectives += [
             obj.GradientObjective(x0, y0, z0, value=0, ion=Ca40),
             obj.HessianObjective(x0, y0, z0, entries='xx', value=axial, ion=Ca40, pseudo=False),
             obj.HessianObjective(x0, y0, z0, entries='yz', value=rtilt, ion=Ca40, pseudo=False),
             obj.VoltageObjective(0)
-        ] for x0 in x
-    ]
+        ]
 
-    global_objectives = [
-        obj.VoltageObjective(10, constraint_type='<='),
-        obj.VoltageObjective(-10, constraint_type='>='),
+    objectives += [
+        obj.VoltageObjective(waveform, 10, constraint_type='<='),
+        obj.VoltageObjective(waveform, -10, constraint_type='>='),
         # obj.SlewRateObjective(4e6, constraint_type='<=')  # V/s
-        obj.SlewRateObjective(weight=1e4)
+        obj.SlewRateObjective(waveform, dt=trap.dt, weight=1e4)
     ]
 
-    # extra_constraints = [
-    #     r2 >= freq_to_curv(2e6)
-    # ]
-    extra_constraints = None
-    waveform, final_costs = solver(trap, step_objectives, global_objectives, extra_constraints, verbose=True)
+    _ = solver(objectives, verbose=True)
     waveform = waveform.value  # get array from cvxpy variable
     print(waveform.shape, waveform[0])
 

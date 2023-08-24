3ce7694de9dffbb65ff93aeff642fee99a61c4db
cmordini <cmordini@phys.ethz.ch>
2023-08-24 19:41:10 +0200

    Fig_transport twocolumns
    Former-commit-id: b3cf848b7da2ea9c8f547d0d0bcdf18d148a8252

--- paper-figures/dodo.py
+++ paper-figures/dodo.py
@@ -20,6 +20,10 @@ sources = Path('sources')
 modules = {m.stem[5:]: import_module(f"{sources}.{m.stem}")
            for m in sources.glob('plot_*.py')}
 
+templates = {
+    'transport': 'sources/fig_transport_template_twocolumns.svg'
+}
+
 fig_format = get_var('format', 'pdf')
 show = get_var('show', False)
 
@@ -28,12 +32,15 @@ def task_plot():
     for name, module in modules.items():
         figname = f"figures/fig_{name}.{fig_format}"
         actions = [(module.plot, [figname], {})]
+        file_dep = [module.__file__, 'plotting/cpc.mplstyle']
         if show:
             actions += [plt.show]
+        if name in templates:
+            file_dep += [templates[name]]
         yield {
             'name': name,
             'actions': actions,
-            'file_dep': [module.__file__, 'plotting/cpc.mplstyle'],
+            'file_dep': file_dep,
             'targets': [figname]
         }
 
--- paper-figures/sources/plot_transport.py
+++ paper-figures/sources/plot_transport.py
@@ -5,9 +5,10 @@
 # Author: Carmelo Mordini <cmordini@phys.ethz.ch>
 
 import numpy as np
+import matplotlib as mpl
 from pathlib import Path
 import json
-
+import subprocess
 import figurefirst as fifi
 
 
@@ -20,14 +21,14 @@ def plot(figname):
     apply_style()
 
     here = Path(__file__).parent
-    layout = fifi.FigureLayout(here / 'fig_transport_template.svg', make_mplfigures=True)
+    layout = fifi.FigureLayout(here / 'fig_transport_template_twocolumns.svg', make_mplfigures=True)
 
     with open(data_dir / 'analysis_transport.json', 'r') as fp:
         results = json.load(fp)
 
     x_eq = np.stack([np.asarray(r['x_eq']) * 1e6 for r in results], axis=0)
 
-    ax = layout.figures['none']['ax']['axis']
+    ax = layout.axes['ax']['axis']
     ax.plot(x_eq[:, 0], 'k')
 
     ax.set(
@@ -38,16 +39,20 @@ def plot(figname):
     trap = SurfaceTrap()
     waveform = np.load(data_dir / 'waveform_transport.npy')
 
-    def _plot_voltages(voltages, axes):
-        _ = plot_voltages_on_trap(trap, voltages, axes=axes, fontsize=2)
-
-        for _ax in axes:
-            _ax.set(
-                xticks=[],
-                yticks=[],
-                xlabel=None,
-                ylabel=None
-            )
+    def _plot_voltages(voltages, axes, fontsize=5):
+        # _style = {'font.size': 5, 'axes.linewidth': 0.5}
+        # with mpl.style.context(_style):
+        _ = plot_voltages_on_trap(trap, voltages, axes=axes, fontsize=0)
+        ax0, ax = axes
+        ax0.set(
+            xticklabels=[],
+            yticklabels=[],
+            xlabel=None,
+            ylabel=None,
+        )
+        ax.tick_params(labelsize=fontsize)
+        ax.xaxis.get_label().set_fontsize(fontsize)
+        ax.yaxis.get_label().set_fontsize(fontsize)
 
     ax0 = layout.figures['fig_start']['ax0']['axis']
     ax = layout.figures['fig_start']['ax']['axis']
@@ -57,4 +62,14 @@ def plot(figname):
     ax = layout.figures['fig_stop']['ax']['axis']
     _plot_voltages(waveform[-1], axes=[ax0, ax])
 
-    layout.save(Path(figname).with_suffix('.svg'))
+    cax = layout.axes['cax']['axis']
+    mpl.colorbar.ColorbarBase(cax, cmap='RdBu_r', norm=mpl.colors.Normalize(vmin=-10, vmax=10))
+    cax.set(
+        yticks=(-10, 0, 10)
+    )
+    cax.yaxis.set_ticks_position('left')
+
+    figname_svg = figname[:-4] + '.svg'
+    layout.save(figname_svg)
+    subprocess.run(["inkscape", figname_svg, "--export-type=pdf", "-o", figname], shell=True)
+    # Path(figname_svg).unlink()
